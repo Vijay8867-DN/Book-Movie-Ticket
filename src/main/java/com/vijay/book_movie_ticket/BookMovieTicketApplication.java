1bdfa8790213d1ae706d44ab0a2913a8e575394c
vijaydn634 <vijaydn634@gamil.com>
2025-11-25 12:31:49 +0530

    Project creation Done

---
+++ src/main/java/com/vijay/book_movie_ticket/BookMovieTicketApplication.java
@@ -0,0 +1,13 @@
+package com.vijay.book_movie_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMovieTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMovieTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vijay/book_movie_ticket/BookMovieTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijay.book_movie_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMovieTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
